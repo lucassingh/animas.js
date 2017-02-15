35c08d162180bd254ff0e2110534c748d0f90374
DESKTOP-3CFV97H\lukas <lucas.singh@globadlity.com>
2017-02-15 09:40:27 -0300

    relese

--- js/animas.js
+++ js/animas.js
@@ -1,7 +1,5 @@
 /*-----------Example 1------------*/
 
-//Add bounceUp effect on Click of the square
-
 $(function(){
 	$('#square0').click(function () {
 		  $(this).effect("bounce", { times:7
@@ -25,10 +23,47 @@ $('#square1').click(function () {
      } , 3000);
   	});
 
-/*-----------Example 3------------*/
+/*-----------Example 4------------*/
 
     $('#square3').click(function () {
     		  $(this).effect("bounce",
           { direction:'left', times:7
          }, 3000);
     	});
+
+/*-----------Example 5------------*/
+
+	$('#square4').click(function() {
+	for(i=0;i<3;i++) {
+		$(this).fadeTo('fast', 0.5).fadeTo('fast', 1.0);
+		}
+	});
+
+	/*-----------Example 6------------*/
+
+	$(document).ready(function() {
+
+				$("#square5").click(function(){
+					 $("#square5").effect( "pulsate", {times:3}, 3000 );
+				});
+
+		 });
+
+	 /*-----------Example 7------------*/
+
+		$(document).ready(function() {
+
+			$("#square6").click(function(){
+				 $("#square6").effect( "shake", {times:4}, 500 );
+			});
+
+	 });
+
+	 /*-----------Example 8------------*/
+
+	 	$(document).click(function() {
+  	$('#square7' ).fadeIn( 3000, function() {
+    $( "#square7" ).fadeIn( 100 );
+  });
+	return false;
+ });
