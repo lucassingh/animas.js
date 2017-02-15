621944c8a786157120774a29d37556cb116f250f
DESKTOP-3CFV97H\lukas <lucas.singh@globadlity.com>
2017-02-15 17:25:56 -0300

    headers

--- js/animas.js
+++ js/animas.js
@@ -42,7 +42,6 @@ $('#square1').click(function () {
 	/*-----------Example 6------------*/
 
 	$(document).ready(function() {
-
 				$("#square5").click(function(){
 					 $("#square5").effect( "pulsate", {times:3}, 3000 );
 				});
@@ -52,18 +51,91 @@ $('#square1').click(function () {
 	 /*-----------Example 7------------*/
 
 		$(document).ready(function() {
-
 			$("#square6").click(function(){
 				 $("#square6").effect( "shake", {times:4}, 500 );
 			});
-
 	 });
 
-	 /*-----------Example 8------------*/
+	/*-----------Example 8------------*/
 
-	 	$(document).click(function() {
-  	$('#square7' ).fadeIn( 3000, function() {
-    $( "#square7" ).fadeIn( 100 );
+	$(document).ready(function(){
+    $(".btn1").click(function(){
+        $('#square7').fadeOut(1000);
+    });
+    $(".btn2").click(function(){
+        $('#square7').fadeIn(1000);
+    });
+	});
+
+	/*-----------Example 9------------*/
+
+	$(document).ready(function(){
+  $('.btn3').click(function(){
+		$('#square-a').delay(500).fadeIn();
+		$('#square-b').delay(900).fadeIn();
+		$('#square-c').delay(1300).fadeIn();
+	  });
+	$('.btn4').click(function(){
+		$('#square-a').hide();
+		$('#square-b').hide();
+		$('#square-c').hide();
+		});
+	});
+
+	/*-----------Example 10------------*/
+
+	$(document).ready(function(){
+    $(".btn5").click(function(){
+        $('#square9').hide();
+    });
+    $(".btn6").click(function(){
+        $('#square9').show();
+	    });
+		});
+
+/*-----------Example 11------------*/
+
+$(document).ready(function() {
+	$('.btn7').click(function() {
+      $('#square10').animate({
+			'marginLeft' : "+=10px" //moves right
+		});
+
+  $('.btn8').click(function() {
+        $('#square10').animate({
+        'marginLeft' : "-=10px" //moves left
+        });
+    	});
+		});
   });
-	return false;
- });
+
+	/*-----------Example 12------------*/
+
+	$(document).ready(function() {
+    $('.btn9').click(function() {
+        $('#square11').animate({
+        'marginTop' : "+=5px"
+        });
+    });
+    $('.btn10').click(function() {
+        $('#square11').animate({
+        'marginTop' : "-=5px"
+        });
+    	});
+		});
+
+	/*-----------Example 13------------*/
+
+	$(document).ready(function(){
+    $('.header-slider').click(function(){
+        $('.content-slider').slideToggle("slow");
+    	});
+		});
+
+	/*-----------Example 14------------*/
+
+	$(document).ready(function(){
+    $('.header-slider-2').click(function(){
+      $('.content-slider-2').animate({width:'toggle'},350);
+    	});
+		});
